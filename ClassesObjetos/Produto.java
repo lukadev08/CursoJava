9b2bc40e2368e2ca411d249f69ce5716f10448ce
lukadev08 <luskaf8@gmail.com>
2023-01-24 15:26:39 -0300

    Commit

---
+++ ClassesObjetos/Produto.java
@@ -0,0 +1,8 @@
+package ClassesObjetos;
+
+public class Produto {
+    
+    String nome;
+    double preco;
+    double desconto;
+}
---
+++ ClassesObjetos/ProdutoTeste.java
@@ -0,0 +1,11 @@
+package ClassesObjetos;
+
+public class ProdutoTeste {
+    public static void main(String[] args) {
+        /*estando no mesmo pacote nao e necessario
+         importar o metodo de outro lugar
+         */
+        
+        
+    }
+}
